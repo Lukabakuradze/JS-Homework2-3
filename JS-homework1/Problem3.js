87a1018155b668e66a427f1777c13356eb365476
Lukabakuradze <104515036+Lukabakuradze@users.noreply.github.com>
2022-07-26 17:27:33 +0400

    Delete Problem3.js

--- JS-homework1/Problem3.js
+++
@@ -1,9 +0,0 @@
-var array = [ 'watermelon', 'pear', 10, 45, 50, 'apple', 'ananas' ];
-
-for (var i=0; i<array.length; i++ ){
-    var element = array[i]
-    if (typeof element == 'string'  ){
-        console.log(element);
-
-    }
-}
