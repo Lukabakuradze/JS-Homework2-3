94a2a333a23d75206b6edb61ce7978b79c3bb34c
Lukabakuradze <104515036+Lukabakuradze@users.noreply.github.com>
2022-07-26 17:24:12 +0400

    Add files via upload

---
+++ JS-Homework 2,3/problem3.js
@@ -0,0 +1,12 @@
+let user = {
+    firstname: 'giorgi',
+    lastname: 'saakadze',
+    age: 32,
+    isloggedin: true
+  }
+
+  if (user.isloggedin == true){
+    console.log('giorgi' + ' ' + 'saakadze' );
+  } else {
+    console.log ('false')
+  }
---
+++ JS-Homework 2,3/problem4.js
@@ -0,0 +1,3 @@
+function FindMax (array){
+    let max  
+}
